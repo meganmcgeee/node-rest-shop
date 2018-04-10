d8ba932bca2382eefd589b580349d28b8b59b83e
meganmcgeee <meganpmcgee@gmail.com>
2018-04-10 10:53:31 -0700

    Added Cross Orgigin Access for  API so other Clients may access HTTP Methods.

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+    'Origin X-Requested-With, Content-Type, Accept, Authorization' 
+  );
+  if(req.method = 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE')
+    return res.status(200).json({});
+  }
+  next()
+});
+
 // Routes should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
